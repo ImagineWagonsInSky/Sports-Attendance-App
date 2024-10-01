058a4baca389c891346f2b0440c17854127b9810
ImagineWagonsInSky <will18.perfect@gmail.com>
2024-10-01 19:08:24 +0100

    Models

--- backend/attendance/models.py
+++ backend/attendance/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Session(models.Model):
+    date = models.DateTimeField()
+    description = models.TextField()
+
+class Attendance(models.Model):
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    is_attending = models.BooleanField(default=False)
